c89ea6ecbd09cff4384b5357789931a04528bb4d
SeongTae Jeong <kevin13@chol.net>
2020-10-12 12:31:55 +0900

    Add hexcode project

---
+++ hexcode/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace hexcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Help();
+                return;
+            }
+
+            string encodingType = args[0];
+
+            if (encodingType == "-l")
+            {
+                foreach (var item in Encoding.GetEncodings())
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                return;
+            }
+
+            if (args.Length < 2)
+            {
+                Help();
+                return;
+            }
+
+            string text = args[1];
+
+            Encoding encoding = Encoding.GetEncoding(encodingType);
+
+            if (text.IndexOf('-') != -1)
+            {
+                string[] textBytes = text.Split('-');
+                Decode(encoding, textBytes);
+            }
+            else if (text.IndexOf(',') != -1)
+            {
+                string[] textBytes = text.Split(',');
+                Decode(encoding, textBytes);
+            }
+            else
+            {
+                Encode(encoding, text);
+            }
+        }
+
+        private static void Help()
+        {
+            string appName = Path.GetFileNameWithoutExtension(typeof(Program).Assembly.Location);
+            Console.WriteLine($"{appName} -l");
+            Console.WriteLine($"{appName} [encoding_type] [text]");
+            Console.WriteLine($"ex:");
+            Console.WriteLine($"\t{appName} utf-8 \"test\"");
+            Console.WriteLine($"\t{appName} utf-8 \"74-65-73-74\"");
+            Console.WriteLine($"\t{appName} utf-8 \"0x74-0x65-0x73-0x74\"");
+            Console.WriteLine($"\t{appName} utf-8 \"74,65,73,74\"");
+            Console.WriteLine($"\t{appName} utf-8 \"0x74,0x65,0x73,0x74\"");
+        }
+
+        private static void Decode(Encoding encoding, string[] textBytes)
+        {
+            List<byte> buffer = new List<byte>();
+            foreach (string ch in textBytes)
+            {
+                string byteText = ch;
+
+                if (byteText.StartsWith("0x") == true)
+                {
+                    byteText = byteText.Substring(2);
+                }
+
+                byte b = byte.Parse(byteText, System.Globalization.NumberStyles.HexNumber);
+                buffer.Add(b);
+            }
+
+            Console.WriteLine(encoding.GetString(buffer.ToArray()));
+        }
+
+        private static void Encode(Encoding encoding, string text)
+        {
+            byte [] buffer = encoding.GetBytes(text);
+            Console.WriteLine(BitConverter.ToString(buffer));
+        }
+    }
+}
